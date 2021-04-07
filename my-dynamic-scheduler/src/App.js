826cec13661b1b70310870eb8353bf56f68d2607
beccascarlett <rebecca.scarlett@hotmail.com>
2021-04-07 14:45:49 -0400

    add css

--- my-dynamic-scheduler/src/App.js
+++ my-dynamic-scheduler/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import Main from './components/Main.js';
 
 
--- my-dynamic-scheduler/src/components/Calendar.jsx
+++ my-dynamic-scheduler/src/components/Calendar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import './styles.css';
 import NavBar from '../components/NavBar.js';
 import Header from '../components/Header.js';
 import { subMonths, addMonths, isSameMonth, startOfWeek, endOfWeek, addDays, format, startOfMonth, endOfMonth, isSameDay} from "date-fns";
