e3416410faa3f1fc2011d2d42e0ea3e9635a7ea4
Joanne Bui <buix9830@mylaurier.ca>
2021-04-08 09:19:18 -0400

    event and task form created

--- my-dynamic-scheduler/src/components/AddEvent.jsx
+++ my-dynamic-scheduler/src/components/AddEvent.jsx
@@ -1,16 +1,75 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
+import { withRouter } from 'react-router-dom';
 
 class AddEvent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            event: '',
+            date: '',
+            time: '',
+            duration: ''
+        };
+    }
+
+    handleEventChange = (e) => {
+        e.preventDefault();
+        this.setState({event: e.target.value});
+    }
+
+    handleDateChange = (e) => {
+        e.preventDefault();
+        this.setState({date: e.target.value});
+    }
+    handleTimeChange = (e) => {
+        e.preventDefault();
+        this.setState({time: e.target.value});
+    }
+
+    handleDurationChange = (e) => {
+        e.preventDefault();
+        this.setState({duration: e.target.value});
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.history.push('/CalendarPage');
+    }
 
     render() {
         return (
-            <Popup trigger={<button>Add Event</button>}>
-                <div>
-                    <h1>Add Event Code Here</h1>
-                </div>
-            </Popup>
+            <div className="addEvent">
+            <h1>ADD EVENT</h1>
+            <form onSubmit={this.handleFormSubmit}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><label>Event: </label></td>
+                            <td><input type="text" value={this.state.event} onChange={this.handleEventChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Date: </label></td>
+                            <td><input type="text" value = {this.state.date} onChange={this.handleDateChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Time: </label></td>
+                            <td><input type="text" value = {this.state.time} onChange={this.handleTimeChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Duration: </label></td>
+                            <td><input type="text" value = {this.state.duration} onChange={this.handleDurationChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><input type="checkbox" value="Recurring" /></td>
+                            <td><label>Recurring Event</label></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button type="submit" value="Add Event">addEvent</button>
+            </form>
+            <div id="successful"></div>
+        </div>
         );
     }
 }
-export default AddEvent
+export default withRouter(AddEvent);
--- my-dynamic-scheduler/src/components/AddTask.jsx
+++ my-dynamic-scheduler/src/components/AddTask.jsx
@@ -1,16 +1,75 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
+import { withRouter } from 'react-router-dom';
 
 class AddTask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            task: '',
+            due_date: '',
+            difficulty: '',
+            duration: ''
+        };
+    }
 
+    handleTaskChange = (e) => {
+        e.preventDefault();
+        this.setState({task: e.target.value});
+    }
+
+    handleDueDateChange = (e) => {
+        e.preventDefault();
+        this.setState({due_date: e.target.value});
+    }
+
+    handleDifficultyChange = (e) => {
+        e.preventDefault();
+        this.setState({difficulty: e.target.value});
+    }
+
+    handleDurationChange = (e) => {
+        e.preventDefault();
+        this.setState({duration: e.target.value});
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.history.push('/CalendarPage');
+    }
     render() {
         return (
-            <Popup trigger={<button>Add Task</button>}>
-                <div>
-                    <h1>Add Task Code Here</h1>
-                </div>
-            </Popup>
+            <div className="addTask">
+            <h1>ADD TASK</h1>
+            <form onSubmit={this.handleFormSubmit}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><label>Task: </label></td>
+                            <td><input type="text" value={this.state.task} onChange={this.handleTaskChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Due Date: </label></td>
+                            <td><input type="text" value={this.state.due_date} onChange={this.handleDueDateChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Difficulty: </label></td>
+                            <td><input type="text" value={this.state.difficulty} onChange={this.handleDifficultyChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Duration: </label></td>
+                            <td><input type="text" value={this.state.duration} onChange={this.handleDurationChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><input type="checkbox" value="Recurring" /></td>
+                            <td><label>Recurring Event</label></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button type="submit" value = "Add Task">addTask</button>
+            </form>
+            <div id="successful"></div>
+            </div>
         );
     }
 }
-export default AddTask
+export default withRouter(AddTask);
